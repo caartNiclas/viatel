279a32ec445ad56a35df075cf6b872a8c6eb4356
nthore <niclasthore@hotmail.com>
2022-06-07 01:05:05 +0200

    Hover

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -17,10 +17,10 @@ export default function Movie({ movie }: MovieProps) {
     <div className="col-md-3" style={{ paddingBottom: "1rem" }} key={movie.title} onClick={() => navigateDetail(movie.id)}>
       <div className="card">
         <img className="card-img-top img-fluid" src={movie.poster_path ? `https://image.tmdb.org/t/p/w220_and_h330_face/${movie.poster_path}` : "http://via.placeholder.com/360x540"} alt={movie.title} />
+
         <div className="card-body">
           <span className="card-title" style={{ display: "flex" }}>
             {movie.title}
-
             <Rating className="ratings" icon="star" defaultRating={movie.vote_average / 2} maxRating={5} />
           </span>
           <span>{movie.release_date}</span>
